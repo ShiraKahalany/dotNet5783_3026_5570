6879b39a0a19f19e42ae91901a119802f226ce03
ShiraKy <shkahala@g.jct.ac.il>
2022-10-26 00:04:08 +0300

    hi

---
+++ Stage0/Program3026.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome3026();
+            Welcome5570();
+            Console.ReadKey();
+        }
+        //hyrrreerrrr
+        static partial void Welcome5570(); 
+
+        private static void Welcome3026()
+        {
+            Console.Write("Enter your name: ");
+            string input = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", input);
+        }
+    }
+}
