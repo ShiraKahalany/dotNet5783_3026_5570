bcb1f5d93e20a97861e1efb1bcc9f8e5ded5d606
Hallel <hallel@g.jct.ac.il>
2022-11-28 15:26:08 +0200

    לל

--- BL/BO/Tools.cs
+++ BL/BO/Tools.cs
@@ -25,4 +25,26 @@ static class Tools
         }
         return str;
     }
+
+
+    public static string CopyFields<T,V>(this T t,V newObject )
+    {
+
+
+        foreach (PropertyInfo prop in t.GetType().GetProperties())
+        {
+            if(prop.Name==)
+
+
+
+            var value = prop.GetValue(t, null);
+            if (value is IEnumerable)
+                foreach (var item in (IEnumerable)value)
+                    str += item.ToStringProperty("   ");
+            else
+                str += "\n" + suffix + prop.Name + ": " + value;
+        }
+        return str;
+    }
+
 }
