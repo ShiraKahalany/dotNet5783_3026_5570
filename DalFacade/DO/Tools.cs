9fbb834d4b2cea1892856d7fa78501d3fa6b6a1c
Hallel <hallel@g.jct.ac.il>
2022-12-20 14:43:03 +0200

    שינוי ללניק - טו סטרינג

--- DalFacade/DO/Tools.cs
+++ DalFacade/DO/Tools.cs
@@ -8,13 +8,18 @@ namespace DO;
 
 public static class Tools
 {
-    public static string ToStringProperty<T>(T t)
-    {
-        string str = "";
-        foreach (PropertyInfo item in t.GetType().GetProperties())
-            str += "\n" + item.Name
-    + ": " + item.GetValue(t, null);
-        return str;
-    }
+    public static string ToStringProperty<T>(this T t) where T : struct =>
+
+    t.GetType().GetProperties()
+     .Aggregate("", (str, prop) => str + "\n" + prop.Name + ": " + prop.GetValue(t, null));
+
+    //public static string ToStringProperty<T>(T t)
+    //{
+    //    string str = "";
+    //    foreach (PropertyInfo item in t.GetType().GetProperties())
+    //        str += "\n" + item.Name
+    //+ ": " + item.GetValue(t, null);
+    //    return str;
+    //}
 
 }
