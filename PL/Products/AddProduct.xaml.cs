b762d8ac9f4eac1e87fcfc6a2b093a64f88409b7
Hallel <hallel@g.jct.ac.il>
2023-01-16 18:15:09 +0200

    addproduct

--- PL/Products/AddProduct.xaml.cs
+++ PL/Products/AddProduct.xaml.cs
@@ -28,11 +28,11 @@ namespace PL.Products
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             string name = InsertName.Text;
-            int id = int.Parse(InserId.Text);
+            //int id = int.Parse(InserId.Text);
             double price = double.Parse(InsertPrice.Text);
             int amount = int.Parse(InsertAmount.Text);
             BO.Category category = (BO.Category)SelectCategory.SelectedItem;
-            BO.Product newproduct = new BO.Product { ID = id, Name = name, Price = price, InStock = amount, Category = category, IsDeleted = false, Path = path };
+            BO.Product newproduct = new BO.Product { ID = 0, Name = name, Price = price, InStock = amount, Category = category, IsDeleted = false, Path = path };
             try
             {
                 bl.Product.AddProduct(newproduct);
