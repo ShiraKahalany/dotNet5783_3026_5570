e96f0680d5b20ebb3fa7bc8d5c11640233aba3b9
Hallel <hallel@g.jct.ac.il>
2023-01-18 18:20:10 +0200

    new func - getOrdersByFilter

--- BL/BlApi/IOrder.cs
+++ BL/BlApi/IOrder.cs
@@ -19,6 +19,8 @@ public interface IOrder
     public void Restore (int id);  //שיחזור הזמנה שבוטלה
     //public BO.Order Restore(int id);   //
     public IEnumerable<BO.OrderForList> GetOrderList(BO.Filters enumFilter = BO.Filters.None, Object? filterValue = null);
+    public IEnumerable<BO.Order> GetOrdersByFilter(BO.Filters enumFilter = BO.Filters.None, Object? filterValue = null);
+
     public BO.Order? UpdateStatusToShipped  (int id);  //עידכון הזמנה ל "שולחה"
     public BO.Order? UpdateStatusToProvided(int id);  //עידכון הזמנה ל "סופקה"
     public BO.OrderTracking? FollowOrder (int id);  //מעקב הזמנה, הצגת השלבים של ההזמנה והתאריכים
